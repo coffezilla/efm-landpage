862e4a1a31d5a2f912fb3da555bbb79240f5ab59
Renato Santos <renatojs.web@gmail.com>
2021-12-31 14:51:21 -0300

    Fixed eslint line too long

--- react-next-ts-landpage/pages/policy-app/index.tsx
+++ react-next-ts-landpage/pages/policy-app/index.tsx
@@ -33,60 +33,157 @@ const PolicyApp: NextPage = () => {
 				</section>
 				<ContentText>
 					<h1>Política de privacidade eFut Manager App</h1>
-					<p><strong>Nossa política de privacidade foi atualiza no dia 29/12/2021.</strong></p>
-					<p>Esta página descreve a nossa política de privacidade e procedimentos de coleta de dados do aplicativo <strong>eFut Manager</strong>. Use este conteúdo para entender os termos legais aplicados ao utilizar o aplicativo <strong>eFut manager.</strong></p>
-					<p>Ao utilizar o aplicativo <strong>eFut Manager</strong> você concorda com todos os ítens listados e descritos nesta página.</p>
+					<p>
+						<strong>Nossa política de privacidade foi atualiza no dia 29/12/2021.</strong>
+					</p>
+					<p>
+						Esta página descreve a nossa política de privacidade e procedimentos de coleta de dados
+						do aplicativo <strong>eFut Manager</strong>. Use este conteúdo para entender os termos
+						legais aplicados ao utilizar o aplicativo <strong>eFut manager.</strong>
+					</p>
+					<p>
+						Ao utilizar o aplicativo <strong>eFut Manager</strong> você concorda com todos os ítens
+						listados e descritos nesta página.
+					</p>
 					<h2>Interpretação e definições</h2>
 					<h3>Interpretação</h3>
-					<p>As palavras em negrito desta sessão são termos utilizados ao longo desta política de privacidade e possuem significado claro.</p>
+					<p>
+						As palavras em negrito desta sessão são termos utilizados ao longo desta política de
+						privacidade e possuem significado claro.
+					</p>
 					<ul>
-					  <li><strong>Aplicativo</strong>: Refere-se ao aplicativo eFut Manager para Android ou iOS.</li>
+						<li>
+							<strong>Aplicativo</strong>: Refere-se ao aplicativo eFut Manager para Android ou iOS.
+						</li>
 					</ul>
 					<ul>
-					  <li><strong>Cookies</strong>: são pequenos arquivos que são armazenados em seu dispositivo (computador, smartphone ou outro dispositivo) por um website e contém detalhes da sua navegação que podem ser utilizados por vários sites em diversas situações.</li>
+						<li>
+							<strong>Cookies</strong>: são pequenos arquivos que são armazenados em seu dispositivo
+							(computador, smartphone ou outro dispositivo) por um website e contém detalhes da sua
+							navegação que podem ser utilizados por vários sites em diversas situações.
+						</li>
 					</ul>
 					<ul>
-					  <li><strong>Nós</strong>: Desenvolvedora do aplicativo, BHX Sites.</li>
+						<li>
+							<strong>Nós</strong>: Desenvolvedora do aplicativo, BHX Sites.
+						</li>
 					</ul>
 					<ul>
-					  <li><strong>Aparelho</strong>: Significa o dispositivo a que o aplicativo eFut Manager foi instalado.</li>
+						<li>
+							<strong>Aparelho</strong>: Significa o dispositivo a que o aplicativo eFut Manager foi
+							instalado.
+						</li>
 					</ul>
 					<ul>
-					  <li><strong>Dados pessoais</strong>: É a informação relacionada ao conteúdo coletado do aparelho.</li>
+						<li>
+							<strong>Dados pessoais</strong>: É a informação relacionada ao conteúdo coletado do
+							aparelho.
+						</li>
 					</ul>
 					<ul>
-					  <li><strong>Você</strong>: O utilizador do aplicativo eFut Manager.</li>
+						<li>
+							<strong>Você</strong>: O utilizador do aplicativo eFut Manager.
+						</li>
 					</ul>
 					<h2>O aplicativo</h2>
-					<p>O <strong>eFut Manager Android e iOS</strong> é um aplicativo grátis feito para iOS e Android. O aplicativo foi desenvolvido para servir como um visualizador de resultados de partidas do jogo online <a href="https://efm.bhxsites.com.br/landpage/">eFut Manager Web</a>.</p>
-					<p>A única funcionalidade do aplicativo é visualizar os dados e resultados do jogo <strong>eFut Manager Web</strong>, portanto não é necessário realizar login ou ter qualquer tipo de cadastro para poder usufruir do aplicativo.</p>
-					<p>Como o aplicativo serve apenas para visualizar os resultados, você NÃO conseguirá editar ou cadastrar nenhum tipo de dado usando o aplicativo.</p>
-					<p>O <strong>eFut Manager</strong> é um jogo de futebol manager online, porém O APLICATIVO NÃO É UM JOGO. O APLICATIVO É APENAS UM MEIO PARA QUE VOCÊ POSSA ACOMPANHAR RESULTADOS DO JOGO ONLINE de uma forma mais prática e rápida.</p>
+					<p>
+						O <strong>eFut Manager Android e iOS</strong> é um aplicativo grátis feito para iOS e
+						Android. O aplicativo foi desenvolvido para servir como um visualizador de resultados de
+						partidas do jogo online{' '}
+						<a href="https://efm.bhxsites.com.br/landpage/">eFut Manager Web</a>.
+					</p>
+					<p>
+						A única funcionalidade do aplicativo é visualizar os dados e resultados do jogo{' '}
+						<strong>eFut Manager Web</strong>, portanto não é necessário realizar login ou ter
+						qualquer tipo de cadastro para poder usufruir do aplicativo.
+					</p>
+					<p>
+						Como o aplicativo serve apenas para visualizar os resultados, você NÃO conseguirá editar
+						ou cadastrar nenhum tipo de dado usando o aplicativo.
+					</p>
+					<p>
+						O <strong>eFut Manager</strong> é um jogo de futebol manager online, porém O APLICATIVO
+						NÃO É UM JOGO. O APLICATIVO É APENAS UM MEIO PARA QUE VOCÊ POSSA ACOMPANHAR RESULTADOS
+						DO JOGO ONLINE de uma forma mais prática e rápida.
+					</p>
 					<h2>Coletando dados</h2>
-					<p>O aplicativo <strong>eFut manager Android e iOS</strong> é apenas um visualizador de resultados do jogo online <strong>eFut Manager Web</strong>, portanto NÃO COLETAMOS NENHUM TIPO DE DADO OU INFORMAÇÃO SUA OU DO SEU DISPOSITIVO.</p>
-					<p>O aplicativo não depende de nenhum de seus dados para poder operar da forma ao qual foi pensado: fornecer os últimos resultados das partidas online do jogo online <strong>eFut Manager Web</strong>.</p>
+					<p>
+						O aplicativo <strong>eFut manager Android e iOS</strong> é apenas um visualizador de
+						resultados do jogo online <strong>eFut Manager Web</strong>, portanto NÃO COLETAMOS
+						NENHUM TIPO DE DADO OU INFORMAÇÃO SUA OU DO SEU DISPOSITIVO.
+					</p>
+					<p>
+						O aplicativo não depende de nenhum de seus dados para poder operar da forma ao qual foi
+						pensado: fornecer os últimos resultados das partidas online do jogo online{' '}
+						<strong>eFut Manager Web</strong>.
+					</p>
 					<h2>Como se cadastrar no eFut Manager?</h2>
-					<p>O aplicativo <strong>eFut Manager</strong> é apenas um aplicativo de suporte, portanto você não conseguirá, dentro do aplicativo, efetuar cadastro ou login. Para fazer parte do jogo <strong>eFut Manager</strong> com login você precisará acessar a versão web do <strong>eFut Manager</strong> e seguir os passos para efetuar cadastro e login.</p>
-					<p>Mesmo depois de efetuar cadastro você não conseguirá utilizar o aplicativo para interagir com sua conta. O aplicativo é apenas para acompanhar resultados, portanto não é possível alterar dados da sua conta <strong>eFut Manager</strong> Web utilizando o aplicativo.</p>
-					<p>Caso queira jogar o jogo online, acesse a página <a href="https://efm.bhxsites.com.br/register">https://efm.bhxsites.com.br/register</a> e crie uma conta online para jogar usando o seu navegador web.</p>
+					<p>
+						O aplicativo <strong>eFut Manager</strong> é apenas um aplicativo de suporte, portanto
+						você não conseguirá, dentro do aplicativo, efetuar cadastro ou login. Para fazer parte
+						do jogo <strong>eFut Manager</strong> com login você precisará acessar a versão web do{' '}
+						<strong>eFut Manager</strong> e seguir os passos para efetuar cadastro e login.
+					</p>
+					<p>
+						Mesmo depois de efetuar cadastro você não conseguirá utilizar o aplicativo para
+						interagir com sua conta. O aplicativo é apenas para acompanhar resultados, portanto não
+						é possível alterar dados da sua conta <strong>eFut Manager</strong> Web utilizando o
+						aplicativo.
+					</p>
+					<p>
+						Caso queira jogar o jogo online, acesse a página{' '}
+						<a href="https://efm.bhxsites.com.br/register">https://efm.bhxsites.com.br/register</a>{' '}
+						e crie uma conta online para jogar usando o seu navegador web.
+					</p>
 					<h2>Deletando minha conta eFut Manager</h2>
-					<p>Como o aplicativo <strong>eFut Manager Android e iOS</strong> não possui login e senha. Para deixar de usar o aplicativo basta desinstalar o aplicativo do seu smartphone.</p>
-					<p>Para deletar sua conta <strong>eFut Manager Web</strong> acesse a página do <strong>eFut Manager</strong> utilizando um navegador web, faça o seu login e acesse as preferências da sua conta. Na página de preferências você terá a opção de “deletar minha conta”.</p>
+					<p>
+						Como o aplicativo <strong>eFut Manager Android e iOS</strong> não possui login e senha.
+						Para deixar de usar o aplicativo basta desinstalar o aplicativo do seu smartphone.
+					</p>
+					<p>
+						Para deletar sua conta <strong>eFut Manager Web</strong> acesse a página do{' '}
+						<strong>eFut Manager</strong> utilizando um navegador web, faça o seu login e acesse as
+						preferências da sua conta. Na página de preferências você terá a opção de “deletar minha
+						conta”.
+					</p>
 					<h2>Privacidade infantil</h2>
-					<p>Nosso aplicativo não é recomendado para pessoas menores de 13 anos. Nós não coletamos nenhum dado de nenhum usuário do aplicativo <strong>eFut Manager</strong>.</p>
+					<p>
+						Nosso aplicativo não é recomendado para pessoas menores de 13 anos. Nós não coletamos
+						nenhum dado de nenhum usuário do aplicativo <strong>eFut Manager</strong>.
+					</p>
 					<h2>Links para outros sites</h2>
-					<p>Nosso aplicativo possui um link para o site <strong>eFut Manager Web</strong> para que os usuários possam criar uma conta e jogar online usando um navegador web. Para mais informações relacionadas ao jogo <strong>eFut Manager online</strong> consulte a página oficial.</p>
+					<p>
+						Nosso aplicativo possui um link para o site <strong>eFut Manager Web</strong> para que
+						os usuários possam criar uma conta e jogar online usando um navegador web. Para mais
+						informações relacionadas ao jogo <strong>eFut Manager online</strong> consulte a página
+						oficial.
+					</p>
 					<h2>Mudanças da nossa política de privacidade</h2>
-					<p>Nós podemos alterar esta política de privacidade de tempos em tempos. Nós não notificamos com antecedência caso haja mudanças.</p>
-					<p>Visite periodicamente nossa política de privacidade para poder certificar-se de que você concorda com nossos termos de utilização do aplicativo <strong>eFut Manager Android e iOS</strong>.</p>
+					<p>
+						Nós podemos alterar esta política de privacidade de tempos em tempos. Nós não
+						notificamos com antecedência caso haja mudanças.
+					</p>
+					<p>
+						Visite periodicamente nossa política de privacidade para poder certificar-se de que você
+						concorda com nossos termos de utilização do aplicativo{' '}
+						<strong>eFut Manager Android e iOS</strong>.
+					</p>
 					<h2>Contate-nos</h2>
-					<p>Se você tem alguma dúvida, crítica ou sugestão relacionada a nossa política de privacidade, entre em contato conosco:</p>
+					<p>
+						Se você tem alguma dúvida, crítica ou sugestão relacionada a nossa política de
+						privacidade, entre em contato conosco:
+					</p>
 					<ul>
-					  <li>Visite nosso site <strong>www.bhxsites.com.br</strong></li>
+						<li>
+							Visite nosso site <strong>www.bhxsites.com.br</strong>
+						</li>
 					</ul>
 					<ul>
-					  <li>Envie-nos um e-mail: <strong>atendimento@bhxsites.com.br</strong></li>
-					</ul>				</ContentText>
+						<li>
+							Envie-nos um e-mail: <strong>atendimento@bhxsites.com.br</strong>
+						</li>
+					</ul>{' '}
+				</ContentText>
 			</main>
 		</div>
 	);
